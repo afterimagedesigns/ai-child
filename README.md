68ca37904219043a4c7ff060d7d97068ea6ad5df
xtianmark <christianmarkpaja@gmail.com>
2016-11-23 21:18:29 +0800

    Remove Unnecessary style

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # AI-theme-child
 
-- Added Recommended plugin list to install
-- Allow SVG to upload
-- Add Style in the page
-- Enhance the design of the child theme..
+- Allow user to upload SVG
--- functions.php
+++ functions.php
@@ -4,30 +4,10 @@
 //  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
 //  
 
-require_once get_stylesheet_directory() . '/includes/plugin-list.php';
-
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
 }
-
-//
-// Your code goes below
-//
-
-/**
- * Enqueue scripts and styles
- */
-function load_style_n_scripts() {
-	// load google font css
-	wp_enqueue_style( 'source-sans-font', '//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i', false );
-}
-add_action( 'wp_enqueue_scripts', 'load_style_n_scripts' );
-
 /**
  * Allow user to upload SVG file
  */
@@ -36,3 +16,8 @@ $mimes['svg'] = 'image/svg+xml';
 return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+//
+// Your code goes below
+//
+
